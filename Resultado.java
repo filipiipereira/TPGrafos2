ce9ef3d746bdac1b8aa33952115315ded0c84ce3
Sofia Grossi <sofiagrossi.gvs@gmail.com>
2025-11-14 15:18:00 -0300

    Arquivo faltando

---
+++ Resultado.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Resultado {
+    public final int raio;
+    public final List<Integer> centros;
+
+    public Resultado(int raio, List<Integer> centros) {
+        this.raio = raio;
+        this.centros = centros;
+    }
+}
